0098b638163268ff7755abd9f2511c19a7f79738
Ali Py <a.pouryousefi98@gmail.com>
2023-08-05 19:51:32 +0330

    add Shabnam font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../styles/styles";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <ToastContainer />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ styles/styles.ts
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Shabnam, sans-serif", // Replace "YourDesiredFont" with the desired font family name
+  },
+});
+
+export default theme;
